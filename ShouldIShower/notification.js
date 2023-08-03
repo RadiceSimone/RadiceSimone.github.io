e4292d6435be91be8782e483dcb58f59cb3cbb4e
Simone Radice <116260363+radicesimone@users.noreply.github.com>
2023-08-03 10:31:01 +0200

    Add files via upload

---
+++ ShouldIShower/notification.js
@@ -0,0 +1,58 @@
+function startNotification() {
+  if (Notification.permission !== "granted") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        scheduleNotifications();
+      }
+    });
+  } else {
+    scheduleNotifications();
+  }
+}
+
+function scheduleNotifications() {
+  const orarioPianificato = parseOrarioInput("10:28");
+  if (orarioPianificato) {
+    setInterval(() => {
+      const now = new Date();
+      if (
+        now.getHours() === orarioPianificato.getHours() &&
+        now.getMinutes() === orarioPianificato.getMinutes()
+      ) {
+        sendNotification();
+      }
+    }, 60000); // Verifica ogni minuto
+  } else {
+    alert("Orario non valido. Inserisci l'orario nel formato HH:MM");
+  }
+}
+
+function parseOrarioInput(input) {
+  const [hours, minutes] = input.split(":").map(Number);
+  if (
+    isNaN(hours) ||
+    isNaN(minutes) ||
+    hours < 0 ||
+    hours > 23 ||
+    minutes < 0 ||
+    minutes > 59
+  ) {
+    return null;
+  }
+  const orarioPianificato = new Date();
+  orarioPianificato.setHours(hours);
+  orarioPianificato.setMinutes(minutes);
+  return orarioPianificato;
+}
+
+function sendNotification() {
+  const options = {
+    body: "È ora di fare il test",
+  };
+
+  const notification = new Notification("Notifica dal Sito", options);
+
+  notification.onclick = () => {
+    window.open("https://www.esempio.com", "_blank");
+  };
+}
