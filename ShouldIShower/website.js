6880dba9678b73315b9be768fd18b74572366044
Simone Radice <116260363+radicesimone@users.noreply.github.com>
2023-08-04 15:58:48 +0200

    Update website.js

--- ShouldIShower/website.js
+++ ShouldIShower/website.js
@@ -89,7 +89,7 @@ async function notifications() {
       if (permission !== 'granted') {
         alert('You need to allow push notifications');
       } else {
-        const orarioPianificato = parseOrarioInput("15:56");
+        const orarioPianificato = parseOrarioInput("16:01");
         
         setInterval(() => {
           const now = new Date();
